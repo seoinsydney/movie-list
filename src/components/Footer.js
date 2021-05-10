2829b4d8b290d5ef0ed74009a368f36d9e322bda
SamiHas <sam_hass14@hotmail.com>
2021-05-10 18:42:21 +1000

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <p>&copy; Movie-list App. All Rights Reserved </p>
+        </div>
+    )
+}
+
+export default Footer
