3adbe3dfe5079c14806cf60ea917bb4366ad125e
SamiHas <sam_hass14@hotmail.com>
2021-05-10 17:57:39 +1000

    created Header without search

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+//import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Movie-list App</h1>
+            <Navbar />
+            {/* <Search /> */}
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV Shows</a></li>
+                <li><a href="#">Actors</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
