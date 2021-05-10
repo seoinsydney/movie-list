8f3f9510717cba05eaa7ef3e3a8dbc8cbe9f471a
SamiHas <sam_hass14@hotmail.com>
2021-05-11 01:28:40 +1000

    Added movie list with dummy data

---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MovieList = (props) => {
+    
+    return (
+       <div> 
+            {props.movies.map((movie) => (
+                <div>
+                    <img src={movie.Poster} alt='movie'></img>
+                    <h3>{movie.Title}</h3>
+                    <p>Released: {movie.Year}</p>
+                </div> 
+            ))}
+            
+       </div> 
+    )
+}
+
+export default MovieList
